27e6af14bd5b827fd0319d4a9476fb2f8f9141ca
Douglas Cardoso <douglascf12@live.com>
2020-03-14 15:50:12 -0300

    pasta bin excluida e classe QuadradoMagico.java resolvida

--- src/Aula03/QuadradoMagico.java
+++ src/Aula03/QuadradoMagico.java
@@ -4,15 +4,43 @@ public class QuadradoMagico {
 
 	public static void main(String[] args) {
 		
+            int[][] M = {
+                        {8, 0, 7},
+                        {4, 5, 6},
+                        {3, 10, 2}
+                    };
+            
+            System.out.println(ehQuadradoMagico(M));
 
 	}
 	
-	//public static boolean ehQuadradoMagico(int[][] Matriz) {
-	//	int somaGeral = 0;
-	//	int somaLinha = 0;
-	//	int somaColuna = 0;
-	//	for (int i = 0; i < Matriz.length; i++) {
-	//		
-	//	}
-	//}
+	public static boolean ehQuadradoMagico(int[][] M) {
+		int somaGeral = 0;
+		int somaDiagPrin = 0;
+		int somaDiagSegun = 0;
+		for (int i = 0; i < M.length; i++) {
+                    int somaLinha = 0;
+                    int somaColuna = 0;
+                    for (int j = 0; j < M[0].length; j++) {
+                        somaLinha += M[i][j];
+                        somaColuna += M[j][i];
+                        if (i == j){
+                            somaDiagPrin += M[i][j];
+                        }
+                        if (i + j == M.length - 1){
+                            somaDiagSegun += M[i][j];
+                        }
+                    }
+                    if (somaGeral == 0){
+                        somaGeral = somaLinha;
+                    }
+                    if (somaGeral != somaLinha || somaGeral != somaColuna){
+                        return false;
+                    }
+		}
+                if (somaGeral != somaDiagPrin || somaGeral != somaDiagSegun){
+                    return false;
+                }
+                return true;
+	}
 }
